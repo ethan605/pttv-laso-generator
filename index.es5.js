64df94e74152556e7613a421493f7559c8ccf553
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-16 22:59:04 +0700

    Update build tool & sample docx

---
+++ index.es5.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _lodash = _interopRequireDefault(require("lodash"));
+
+var _docx4js = _interopRequireDefault(require("docx4js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.debug('Hello world!', _lodash.default.range(3), _docx4js.default);
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+import _ from 'lodash';
+import docx4js from 'docx4js';
 
+console.debug('Hello world!', _.range(3), docx4js);
