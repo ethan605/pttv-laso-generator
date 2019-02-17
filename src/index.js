0b22063b967766043020b3be07374de06cf88cd2
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-17 21:39:53 +0700

    Update csv file structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import fs from 'fs';
 
 import _ from 'lodash';
+import axios from 'axios';
 import chalk from 'chalk';
 import csv from 'csv';
 import docxWasm from '@nativedocuments/docx-wasm';
 import docxTemplates from 'docx-templates';
+
+const BIRTH_HOURS_MAPPING = {
+  tys: 'Tý',
+  suu: 'Sửu',
+  dan: 'Dần',
+  mao: 'Mão',
+  thin: 'Thìn',
+  tyj: 'Tỵ',
+  ngo: 'Ngọ',
+  mui: 'Mùi',
+  than: 'Thân',
+  dau: 'Dậu',
+  tuat: 'Tuất',
+  hoi: 'Hợi',
+};
+
+const BIRTH_HOURS_CONVERSION = {
+  tys: '00',
+  suu: '02',
+  dan: '04',
+  mao: '06',
+  thin: '08',
+  tyj: '10',
+  ngo: '12',
+  mui: '14',
+  than: '16',
+  dau: '18',
+  tuat: '20',
+  hoi: '22',
+};
+
+const GENDERS_MAPPING = { female: 'Nữ', male: 'Nam' };
 
 const LASO_IMAGE_CONFIGS = { extension: '.jpg', height: 16.93, width: 12.7 };
 
@@ -19,7 +52,7 @@ function parseQuestions(rawQuestions) {
   return _.map(pairs, ([question, answer], index) => ({ answer, title: `${index + 1}. ${question}` }));
 }
 
-function parseCsvRecords(records) {
+function parseCsvData(records) {
   return _.map(records, rec => {
     const [
       id,
@@ -30,7 +63,6 @@ function parseCsvRecords(records) {
       birthDay,
       birthMonth,
       birthYear,
-      calendar,
       explanation,
       ...rawQuestions
     ] = rec;
@@ -39,22 +71,23 @@ function parseCsvRecords(records) {
     const questions = parseQuestions(rawQuestions);
 
     return {
+      birthDay,
       birthHour,
-      calendar,
+      birthMonth,
+      birthYear,
       contactDetail,
       fullName,
       gender,
       id,
       mainParagraphs,
       questions,
-      birthDate: `${birthDay}/${birthMonth}/${birthYear}`,
     };
   });
 }
 
-function loadExplanationsList() {
+function loadRecords() {
   return new Promise((resolve, reject) => {
-    const input = String(fs.readFileSync('./input/explanations_list.csv'));
+    const input = String(fs.readFileSync('./input.csv'));
 
     csv.parse(input, (error, rawRecords) => {
       if (error != null) {
@@ -63,12 +96,21 @@ function loadExplanationsList() {
       }
 
       const records = _.slice(rawRecords, 1);
-      resolve(parseCsvRecords(records));
+      resolve(parseCsvData(records));
     });
   });
 }
 
-async function generateDocx(id, data) {
+async function generateDocx(id, record) {
+  const { birthDay, birthHour, birthMonth, birthYear, gender, ...rest } = record;
+  
+  const data = {
+    ...rest,
+    birthDate: `${birthDay}/${birthMonth}/${birthYear}`,
+    birthHour: BIRTH_HOURS_MAPPING[birthHour],
+    gender: GENDERS_MAPPING[gender],
+  };
+
   docxTemplates({
     data,
     additionalJsContext: {
@@ -99,16 +141,37 @@ async function convertToPdf(id) {
 
 async function generateRecords() {
   try {
-    const allExplanations = await loadExplanationsList();
+    const allRecords = await loadRecords();
+    renderLasoImage(allRecords[0]);
 
-    _.each(allExplanations, data => {
-      const { id } = data;
-      generateDocx(id, data);
-      // convertToPdf(id);
-    });
+    // _.each(allRecords, record => {
+    //   const { id, ...data } = record;
+    //   generateDocx(id, data);
+    //   convertToPdf(id);
+    // });
   } catch (error) {
     console.error(chalk.redBright(error.message));
   }
+}
+
+async function renderLasoImage(data) {
+  const { birthDay, birthHour, birthMonth, birthYear, gender, id } = data;
+  const body = {
+    ho_ten: id,
+    gioi_tinh: gender === 'male' ? '1' : '0',
+    loai_lich: '1',
+    ngay_duong: birthDay,
+    thang_duong: birthMonth,
+    nam_duong: birthYear,
+    gio_duong: BIRTH_HOURS_CONVERSION[birthHour],
+    phut_duong: '00',
+    nam_xem: '2019',
+    anh_mau: '1',
+    luutru: '1',
+  };
+
+  const response = await axios.post('https://tuvivietnam.vn/index.php?anlaso/laso', body);
+  console.debug(response);
 }
 
 generateRecords();
