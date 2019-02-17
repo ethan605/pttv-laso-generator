7a17bd4658f7bcb837e3649fa56f6e19b0055c41
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-17 10:30:06 +0700

    Add laso_placeholder.jpg

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 import chalk from 'chalk';
+import docx4js from 'docx4js';
 import Docxtemplater from 'docxtemplater';
 import JSZip from 'jszip';
 
-async function loadTemplate() {
+async function fillTemplate() {
   try {
-    // const content = fs.readFileSync(path.resolve(__dirname, 'template.docx'), 'binary');
     const content = fs.readFileSync(path.resolve('./template.docx'), 'binary');
     const zip = new JSZip(content);
     const doc = new Docxtemplater();
@@ -21,7 +21,6 @@ async function loadTemplate() {
       calendar: 'Âm lịch',
       contact_type: 'Số điện thoại',
       contact_detail: '0652455478',
-      laso_image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QIJBywfp3IOswAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAkUlEQVQY052PMQqDQBREZ1f/d1kUm3SxkeAF/FdIjpOcw2vpKcRWCwsRPMFPsaIQSIoMr5pXDGNUFd9j8TOn7kRW71fvO5HTq6qqtnWtzh20IqE3YXtL0zyKwAROQLQ5l/c9gHjfKK6wMZjADE6s49Dver4/smEAc2CuqgwAYI5jU9NcxhHEy60sni986H9+vwG1yDHfK1jitgAAAABJRU5ErkJggg==',
       explaination_detail: 'Đương số tuổi Ngọ',
       questions: [
         {
@@ -38,17 +37,20 @@ async function loadTemplate() {
     doc.render();
 
     const buffer = doc.getZip().generate({ type: 'nodebuffer' });
-    // fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buffer);
     fs.writeFileSync(path.resolve('./output.docx'), buffer);
   } catch (error) {
-    const detail = {
-      message: error.message,
-      name: error.name,
-      stack: error.stack,
-      properties: error.properties,
-    };
-    console.error(chalk.redBright('ERROR: ', JSON.stringify(detail, null, 2)));
+    console.error(chalk.redBright(error.message));
   }
 }
 
-loadTemplate();
+async function loadTemp2() {
+  try {
+    const docx = await docx4js.load('./template.docx');
+    debugger;
+  } catch (error) {
+    console.error(chalk.redBright(error.message));
+  }
+}
+
+// fillTemplate();
+loadTemp2();
