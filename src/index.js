5003662db9aa2d8a28e9f110d450f66fe6e95b8a
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-17 14:37:09 +0700

    Use docx-templates

--- src/index.js
+++ src/index.js
@@ -1,56 +1,39 @@
-import fs from 'fs';
-import path from 'path';
-
 import chalk from 'chalk';
-import docx4js from 'docx4js';
-import Docxtemplater from 'docxtemplater';
-import JSZip from 'jszip';
+import generateDocx from 'docx-templates';
 
-async function fillTemplate() {
+function fillTemplate() {
   try {
-    const content = fs.readFileSync(path.resolve('./template.docx'), 'binary');
-    const zip = new JSZip(content);
-    const doc = new Docxtemplater();
-    doc.loadZip(zip);
-
-    doc.setData({
-      full_name: 'Nguyễn Văn A',
-      gender: 'Nam',
-      birth_hour: 'Ngọ',
-      birth_date: '12/08/Đinh Sửu',
-      calendar: 'Âm lịch',
-      contact_type: 'Số điện thoại',
-      contact_detail: '0652455478',
-      explaination_detail: 'Đương số tuổi Ngọ',
-      questions: [
-        {
-          title: '1. Hỏi năm nay làm ăn được không?',
-          answer: 'Năm nay làm ăn tốt',
-        },
-        {
-          title: '2. Gia đình sức khoẻ tốt không?',
-          answer: 'Năm nay làm ăn tốt',
-        },
-      ],
+    generateDocx({
+      template: './template.docx',
+      output: './output.docx',
+      cmdDelimiter: '~',
+      additionalJsContext: {
+        laso_image: () => ({ width: 12.7, height: 16.93, path: './laso_placeholder.jpg', extension: '.jpg' }),
+      },
+      data: {
+        full_name: 'Nguyễn Văn A',
+        gender: 'Nam',
+        birth_hour: 'Ngọ',
+        birth_date: '12/08/Đinh Sửu',
+        calendar: 'Âm lịch',
+        contact_type: 'Số điện thoại',
+        contact_detail: '0652455478',
+        explaination_detail: 'Đương số tuổi Ngọ',
+        questions: [
+          {
+            title: '1. Hỏi năm nay làm ăn được không?',
+            answer: 'Năm nay làm ăn tốt',
+          },
+          {
+            title: '2. Gia đình sức khoẻ tốt không?',
+            answer: 'Năm nay làm ăn tốt',
+          },
+        ],
+      },
     });
-
-    doc.render();
-
-    const buffer = doc.getZip().generate({ type: 'nodebuffer' });
-    fs.writeFileSync(path.resolve('./output.docx'), buffer);
   } catch (error) {
-    console.error(chalk.redBright(error.message));
+    console.error(chalk.red(error.message));
   }
 }
 
-async function loadTemp2() {
-  try {
-    const docx = await docx4js.load('./template.docx');
-    debugger;
-  } catch (error) {
-    console.error(chalk.redBright(error.message));
-  }
-}
-
-// fillTemplate();
-loadTemp2();
+fillTemplate();
