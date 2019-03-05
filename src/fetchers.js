826590c32996e2a4cd5ff709ad4c2096b643127f
Ethan Nguyen <thanhnx.605@gmail.com>
2019-03-05 13:51:21 +0700

    Remove unused files

--- src/fetchers.js
+++ src/fetchers.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import qs from 'qs';
 
-import {
-  /* BASE64_IMAGE_PREFIX, */ CHART_GENERATOR_URL,
-  CORS_ANYWHERE_URL,
-  HOURS_CONVERSION,
-} from './constants';
+import { CHART_GENERATOR_URL, HOURS_CONVERSION } from './constants';
 
 /* eslint-disable import/prefer-default-export */
 
@@ -29,20 +25,10 @@ export async function fetchChartImage(record) {
     thang_duong: birthMonth,
   };
 
-  const { data: pageHtml } = await axios.post(
-    CORS_ANYWHERE_URL + CHART_GENERATOR_URL,
-    qs.stringify(body),
-    { Origin: 'null' },
-  );
+  const { data: pageHtml } = await axios.post(CHART_GENERATOR_URL, qs.stringify(body));
   const selector = cheerio.load(pageHtml);
   const imageLink = selector('div.content_wrap_laso > img')[0].attribs.src;
-  const imageUrl = new URL(imageLink);
-  return imageUrl.origin + imageUrl.pathname;
-
-  // const { data: pageHtml } = await axios.post(CHART_GENERATOR_URL, qs.stringify(body));
-  // const selector = cheerio.load(pageHtml);
-  // const imageLink = selector('div.content_wrap_laso > img')[0].attribs.src;
-  // const { data: imageData } = await axios.get(imageLink, { responseType: 'arraybuffer' });
-  // const imageBase64 = Buffer.alloc(imageData.byteLength, imageData, 'binary').toString('base64');
-  // return `data:image/png;base64,${imageBase64}`;
+  const { data: imageData } = await axios.get(imageLink, { responseType: 'arraybuffer' });
+  const imageBase64 = Buffer.alloc(imageData.byteLength, imageData, 'binary').toString('base64');
+  return `data:image/png;base64,${imageBase64}`;
 }
