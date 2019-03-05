932d6494a0cac15ed867ced119d2c9ed8fc8ad01
Ethan Nguyen <thanhnx.605@gmail.com>
2019-03-05 14:05:59 +0700

    Refine

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+import fs from 'fs';
 
+import _ from 'lodash';
+
+import { renderChartReading } from './renderers';
+import { parseCsv } from './parsers';
+
+async function parseAndGenerate() {
+  try {
+    const csvData = fs.readFileSync('./input.csv', 'utf-8');
+    const records = await parseCsv(csvData);
+
+    _.each(records, async record => {
+      const { checksum, id } = record;
+      const report = await renderChartReading(record);
+      fs.writeFileSync(`./outputs/${id}_${checksum.substring(checksum.length - 6)}.html`, report, 'utf-8');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+parseAndGenerate();
